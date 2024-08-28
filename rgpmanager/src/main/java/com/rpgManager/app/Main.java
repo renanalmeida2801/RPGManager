dd236aa661c750212a58fd2ac4d953ec73307e09
Renan Almeida <renanvictoralmeidasilva@gmail.com>
2024-08-28 14:58:42 -0300

    resolvendo-conflitos

--- rgpmanager/src/main/java/com/rpgManager/app/Main.java
+++ rgpmanager/src/main/java/com/rpgManager/app/Main.java
@@ -9,35 +9,8 @@ import com.rpgManager.view.PersonagemView;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println(Paths.get("").toAbsolutePath().toString());
-        PersonagemView persona = new PersonagemView();
-
-        persona.mostrarMenu();
-
-        // PersonagemDAOImpl banco = new PersonagemDAOImpl();
-
-        // String nome = "TCK The dirty";
-        // String raca = "O SUJO";
-        // String classe = "Duelista";
-        // char sexo = 'Y';
-        // int nivel = 1000;
-        // List<String> habilidades = List.of(
-        // "Espada Longa",
-        // "Arco e Flecha",
-        // "Cura Rápida",
-        // "Camuflagem",
-        // "Acrobacia",
-        // "Escudo de Fogo");
-
-        // Personagem persona = new Personagem(nome, raca, classe, sexo, nivel,
-        // habilidades);
-
-        // banco.listarTodosPersonagens();
-        // // banco.salvarPersonagem(persona);
-        // System.out.println(banco.buscarPersonagem(10).getNome());
-        // banco.listarTodosPersonagens();
-        // banco.atualizarPersonagem(10);
+        PersonagemView menu = new PersonagemView();
+        menu.mostrarMenu();
 
     }
 }
